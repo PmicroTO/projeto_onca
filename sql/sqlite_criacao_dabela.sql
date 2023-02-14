646d2af9f427a1a04ed8660820ef960ef111bfc3
Lucio Araujo <iyw6gc@gmail.com>
2023-02-14 12:22:58 -0300

    melhor relacionar cada livro `a um usuario na tabela livros caso aja emprestimo`

--- sql/sqlite_criacao_dabela.sql
+++ sql/sqlite_criacao_dabela.sql
@@ -1,45 +1,34 @@
-DROP TABLE IF EXISTS livros;
-DROP TABLE IF EXISTS usuarios;
-
--- Habilitar chaves estrangeiras para correlacionar livros com o cliente
-PRAGMA foreign_keys = ON;
-
--- Criacao da tabela para o modulo livros
-CREATE TABLE livros (
-	cod_liv INTEGER PRIMARY KEY AUTOINCREMENT,
-	isbn INTEGER(13) NOT NULL,
-	titulo TEXT(50) NOT NULL,
-	autores TEXT(255) NOT NULL,
-	ano INTEGER(4) NOT NULL,
-	editora TEXT(50) NOT NULL,
-	categoria TEXT(50) NOT NULL,
-	disponivel NUMERIC(1) NOT NULL,
-	data_add TEXT(10) NOT NULL
+-- excluir tabela livros caso ela exista.
+drop table if exists livros;
+-- excluir tabela usuarios caso ela exista.
+drop table if exists usuarios;
+-- habilitar chaves estrangeiras para correlacionar livros com o cliente
+pragma foreign_keys = on;
+-- criacao da tabela para o modulo usuarios
+create table usuarios (
+	cod_usu integer primary key autoincrement,
+	username text(8) not null,
+	senha text(24) not null,
+	email text(55) not null,
+	nome text(55) not null,
+	cpf integer not null,
+	telefone integer not null,
+	cep integer not null,
+	bairro text(255) not null,
+	rua text(255) not null,
+	casa text(10) not null
 );
-
--- Criacao da tabela para o modulo usuarios
-CREATE TABLE usuarios (
-	cod_usu INTEGER PRIMARY KEY AUTOINCREMENT,
-	username TEXT(8) NOT NULL,
-	senha TEXT(24) NOT NULL,
-	email TEXT(55) NOT NULL,
-	nome TEXT(55) NOT NULL,
-	cpf INTEGER NOT NULL,
-	telefone INTEGER NOT NULL,
-	cep INTEGER NOT NULL,
-	bairro TEXT(255) NOT NULL,
-	rua TEXT(255) NOT NULL,
-	casa TEXT(10) NOT NULL,
-	livro_1 INTEGER,
-	livro_1_data TEXT(10),
-	livro_2 INTEGER,
-	livro_2_data TEXT(10),
-	livro_3 INTEGER,
-	livro_3_data TEXT(10),
-	FOREIGN KEY (livro_1) REFERENCES livros(cod_liv),
-	FOREIGN KEY (livro_2) REFERENCES livros(cod_liv),
-	FOREIGN KEY (livro_3) REFERENCES livros(cod_liv)
+-- criacao da tabela para o modulo livros
+create table livros (
+	cod_liv integer primary key autoincrement,
+	isbn integer(13) not null,
+	titulo text(50) not null,
+	autores text(255) not null,
+	ano integer(4) not null,
+	editora text(50) not null,
+	categoria text(50) not null,
+	emprestimo_usuario integer,
+	emprestimo_data text(10),
+	data_add text(10) not null,
+	foreign key (emprestimo_usuario) references usuarios(cod_usu)
 );
-
-
-
